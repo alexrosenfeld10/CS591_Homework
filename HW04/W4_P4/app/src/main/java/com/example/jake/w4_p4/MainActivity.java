15c2344eaacdce6d5c1042d59304b0e16a3a94bf
Jake Dharmasiri <jschadaway@gmail.com>
2017-10-01 05:40:44 -0400

    Implement word resource arrays and function to process guesses

--- HW04/W4_P4/app/src/main/java/com/example/jake/w4_p4/MainActivity.java
+++ HW04/W4_P4/app/src/main/java/com/example/jake/w4_p4/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.jake.w4_p4;
 
-        import android.content.DialogInterface;
+        import android.content.res.TypedArray;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
-        import android.view.Gravity;
+        import android.util.Log;
         import android.view.View;
         import android.widget.Button;
         import android.widget.ImageView;
         import android.widget.LinearLayout;
         import android.widget.TextView;
+        import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +21,16 @@ public class MainActivity extends AppCompatActivity {
     private Button newGameButton;
 
     private char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-    private Button[] letterButtons = new Button[26];
-    private boolean firstGame = true;
+    private static Button[] letterButtons = new Button[26];
+    private static boolean firstGame = true;
+    private static String wordToSolve;
+    private static String wordToDisplay;
+    private static int lives = 5;
+    private static String hint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(logFlag, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -45,66 +51,125 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if (firstGame) {
-            int letterCount = 0;
-            for (char letter : letters) {
-                final Button b = new Button(this);
-                b.setTag(String.valueOf(letter));
-                b.setSaveEnabled(true);
+        int letterCount = 0;
 
-                LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
-                p.width = 120;
-                p.weight = 0f;
+        for (final char letter : letters) {
+            final Button b = new Button(this);
+            b.setTag(String.valueOf(letter));
+            b.setSaveEnabled(true);
 
-                b.setLayoutParams(p);
-                b.setText(String.valueOf(letter));
+            LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            p.width = 120;
+            p.weight = 0f;
 
-                View.OnClickListener letterClickListener = new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        b.getText();
-                        b.setEnabled(false);
-                    }
-                };
-                b.setOnClickListener(letterClickListener);
+            b.setLayoutParams(p);
+            b.setText(String.valueOf(letter));
 
-                if (letterCount < 7) {
-                    letterRow1.addView(b);
-                } else if (letterCount < 14) {
-                    letterRow2.addView(b);
-                } else if (letterCount < 21) {
-                    letterRow3.addView(b);
-                } else {
-                    letterRow4.addView(b);
+            View.OnClickListener letterClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    processGuess(letter);
+                    b.setEnabled(false);
                 }
-                letterButtons[letterCount] = b;
-                letterCount++;
+            };
+            b.setOnClickListener(letterClickListener);
+
+            if (letterCount < 7) {
+                letterRow1.addView(b);
+            } else if (letterCount < 14) {
+                letterRow2.addView(b);
+            } else if (letterCount < 21) {
+                letterRow3.addView(b);
+            } else {
+                letterRow4.addView(b);
             }
-            //setUpNewGame();
+            letterButtons[letterCount] = b;
+            letterCount++;
+        }
+
+        if (savedInstanceState != null) {
+            for (Button b : letterButtons) {
+                b.setEnabled(savedInstanceState.getBoolean((String) b.getTag()));
+            }
+            wordToSolve = savedInstanceState.getString("WORD_SOLVED_KEY");
+            wordToDisplay = savedInstanceState.getString("WORD_DISPLAY_KEY");
+            wordView.setText(wordToDisplay);
+            hint = savedInstanceState.getString("HINT_KEY");
+            if (hintView != null) {
+                hintView.setText("HINT: " + hint);
+            }
+            firstGame = savedInstanceState.getBoolean("FIRST_GAME");
+            lives = savedInstanceState.getInt("LIVES_LEFT");
+        }
+
+        if (firstGame) {
+            setUpNewGame();
             firstGame = false;
         }
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        for (Button b : letterButtons) {
-            b.setEnabled(savedInstanceState.getBoolean((String) b.getTag()));
-        }
-    }
-
-    @Override
     protected void onSaveInstanceState(Bundle outState) {
+        Log.i(logFlag, "onSaveInstanceState");
         for (Button b : letterButtons) {
             outState.putBoolean((String) b.getTag(), b.isEnabled());
         }
+        outState.putString("WORD_SOLVED_KEY", wordToSolve);
+        outState.putString("WORD_DISPLAY_KEY", wordToDisplay);
+        outState.putString("HINT_KEY", hint);
+        outState.putBoolean("FIRST_GAME", firstGame);
+        outState.putInt("LIVES_LEFT", lives);
         super.onSaveInstanceState(outState);
+    }
+
+    private void processGuess(char letter) {
+        String response;
+        boolean correct = false;
+        for (int i = 0; i < wordToSolve.length(); i++) {
+            if (wordToSolve.charAt(i) == letter) {
+                char[] wordArray = wordToDisplay.toCharArray();
+                wordArray[i * 2] = letter;
+                wordToDisplay = String.valueOf(wordArray);
+                wordView.setText(wordToDisplay);
+                correct = true;
+            }
+        }
+        if (!correct) {
+            lives--;
+            if (lives == 0) {
+                response = "No more guesses. Game over!";
+                for (Button b : letterButtons) {
+                    b.setEnabled(false);
+                }
+            } else {
+                response = "Wrong! Try again.";
+            }
+        } else {
+            response = "Correct!";
+        }
+        Toast toast = Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     private void setUpNewGame() {
         for (Button b : letterButtons) {
             b.setEnabled(true);
         }
+
+        TypedArray words = getResources().obtainTypedArray(R.array.hangman_words);
+        int index = (int) (Math.random() * words.length());
+        int arrayId = words.getResourceId(index, R.array.word0);
+        String[] word = getResources().getStringArray(arrayId);
+
+        wordToSolve = word[0];
+        hint = word[1];
+        wordToDisplay = new String(new char[wordToSolve.length()]).replace("\0", "_");
+        wordToDisplay = wordToDisplay.replace("", " ").trim();
+        wordView.setText(wordToDisplay);
+        if (hintView != null) {
+            hintView.setText("HINT: " + word[1]);
+        }
+        lives = 5;
     }
 
 }
