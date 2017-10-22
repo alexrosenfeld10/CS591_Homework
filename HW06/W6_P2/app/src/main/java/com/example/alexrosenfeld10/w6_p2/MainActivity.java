56f6f6d2af3b520449aeab0185556876534f204d
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2017-10-22 18:07:16 -0400

    typo in xml

---
+++ HW06/W6_P2/app/src/main/java/com/example/alexrosenfeld10/w6_p2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.alexrosenfeld10.w6_p2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+//public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity implements ControlFragment.ControlFragmentListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//Honoring our promise to implement sendMessage.
+    @Override
+    public void sendMessage(String msg) {
+        BottomFragment receivingFragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomFragment);
+        receivingFragment.setFunnyMessage(msg);
+    }
+
+
+
+}
+
+
+//Toast.makeText(getBaseContext(),"I would like to propose a Toast.", Toast.LENGTH_LONG).show();
