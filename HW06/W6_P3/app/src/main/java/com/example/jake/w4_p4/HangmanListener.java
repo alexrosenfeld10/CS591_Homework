531866208487060330c2a891dcd2d8b2bf00de36
Jake Dharmasiri <jschadaway@gmail.com>
2017-10-23 06:25:45 -0400

    Draw the rest of the owl

---
+++ HW06/W6_P3/app/src/main/java/com/example/jake/w4_p4/HangmanListener.java
@@ -0,0 +1,6 @@
+package com.example.jake.w4_p4;
+
+public interface HangmanListener {
+    public void setUpNewGame();
+    public void processGuess(char letter);
+}
--- HW06/W6_P3/app/src/main/java/com/example/jake/w4_p4/MainActivity.java
+++ HW06/W6_P3/app/src/main/java/com/example/jake/w4_p4/MainActivity.java
@@ -1,32 +1,21 @@
 package com.example.jake.w4_p4;
 
-        import android.content.res.TypedArray;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.ImageView;
-        import android.widget.LinearLayout;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.content.res.TypedArray;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HangmanListener {
 
-    private static final String logFlag = "W4_P4";  // Marker for logging events from the app.
+    private static final String logFlag = "W6_P3";  // Marker for logging events from the app.
 
-    private ImageView hangmanView;
-    private TextView wordView, hintView;
-    private LinearLayout letterRow1, letterRow2, letterRow3, letterRow4;
-    private Button newGameButton;
-
-    private char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-    private static Button[] letterButtons = new Button[26];
+    GameFragment gameFragment;
+    HintFragment hintFragment;
+    LettersFragment lettersFragment;
+    private static String wordToSolve, wordToDisplay, hint;
     private static boolean firstGame = true;
-    private static String wordToSolve;
-    private static String wordToDisplay;
     private static int lives = 6;
-    private static String hint;
     private static Toast toast;
 
     @Override
@@ -35,73 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up view references
-        hangmanView = (ImageView) findViewById(R.id.hangmanView);
-        wordView = (TextView) findViewById(R.id.wordView);
-        hintView = (TextView) findViewById(R.id.hintView);
-        letterRow1 = (LinearLayout) findViewById(R.id.letterRow1);
-        letterRow2 = (LinearLayout) findViewById(R.id.letterRow2);
-        letterRow3 = (LinearLayout) findViewById(R.id.letterRow3);
-        letterRow4 = (LinearLayout) findViewById(R.id.letterRow4);
-        newGameButton = (Button) findViewById(R.id.newGameButton);
-
-        newGameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setUpNewGame();
-            }
-        });
-
-        int letterCount = 0;
-
-        for (final char letter : letters) {
-            final Button b = new Button(this);
-            b.setTag(String.valueOf(letter));
-            b.setSaveEnabled(true);
-
-            LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
-            p.width = 120;
-            p.weight = 0f;
-
-            b.setLayoutParams(p);
-            b.setText(String.valueOf(letter));
-
-            View.OnClickListener letterClickListener = new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    processGuess(letter);
-                    b.setEnabled(false);
-                }
-            };
-            b.setOnClickListener(letterClickListener);
-
-            if (letterCount < 7) {
-                letterRow1.addView(b);
-            } else if (letterCount < 14) {
-                letterRow2.addView(b);
-            } else if (letterCount < 21) {
-                letterRow3.addView(b);
-            } else {
-                letterRow4.addView(b);
-            }
-            letterButtons[letterCount] = b;
-            letterCount++;
-        }
+        gameFragment = (GameFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_game);
+        hintFragment = (HintFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_hint);
+        lettersFragment = (LettersFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_letters);
 
         if (savedInstanceState != null) {
-            for (Button b : letterButtons) {
-                b.setEnabled(savedInstanceState.getBoolean((String) b.getTag()));
-            }
             wordToSolve = savedInstanceState.getString("WORD_SOLVED_KEY");
             wordToDisplay = savedInstanceState.getString("WORD_DISPLAY_KEY");
-            wordView.setText(wordToDisplay);
+            gameFragment.setDisplayWord(wordToDisplay);
             hint = savedInstanceState.getString("HINT_KEY");
-            if (hintView != null) {
-                hintView.setText("HINT: " + hint);
-            }
+            hintFragment.setHint("HINT: " + hint);
             firstGame = savedInstanceState.getBoolean("FIRST_GAME");
             lives = savedInstanceState.getInt("LIVES_LEFT");
-            setHangmanImage();
+            gameFragment.setHangmanImage(lives);
         }
 
         if (firstGame) {
@@ -113,9 +48,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         Log.i(logFlag, "onSaveInstanceState");
-        for (Button b : letterButtons) {
-            outState.putBoolean((String) b.getTag(), b.isEnabled());
-        }
         outState.putString("WORD_SOLVED_KEY", wordToSolve);
         outState.putString("WORD_DISPLAY_KEY", wordToDisplay);
         outState.putString("HINT_KEY", hint);
@@ -124,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
-    private void processGuess(char letter) {
+    public void processGuess(char letter) {
         String response;
         boolean gameEnd = false;
         boolean correct = false;
@@ -133,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 char[] wordArray = wordToDisplay.toCharArray();
                 wordArray[i * 2] = letter;
                 wordToDisplay = String.valueOf(wordArray);
-                wordView.setText(wordToDisplay);
+                gameFragment.setDisplayWord(wordToDisplay);
                 correct = true;
             }
         }
@@ -152,13 +84,11 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 response = "Wrong! Try again.";
             }
-            setHangmanImage();
+            gameFragment.setHangmanImage(lives);
         }
 
         if (gameEnd) {
-            for (Button b : letterButtons) {
-                b.setEnabled(false);
-            }
+            lettersFragment.disableButtons();
         }
         if (toast != null) {
             toast.cancel();
@@ -167,10 +97,8 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    private void setUpNewGame() {
-        for (Button b : letterButtons) {
-            b.setEnabled(true);
-        }
+    public void setUpNewGame() {
+        lettersFragment.resetButtons();
 
         TypedArray words = getResources().obtainTypedArray(R.array.hangman_words);
         int index = (int) (Math.random() * words.length());
@@ -181,41 +109,11 @@ public class MainActivity extends AppCompatActivity {
         hint = word[1];
         wordToDisplay = new String(new char[wordToSolve.length()]).replace("\0", "_");
         wordToDisplay = wordToDisplay.replace("", " ").trim();
-        wordView.setText(wordToDisplay);
-        if (hintView != null) {
-            hintView.setText("HINT: " + word[1]);
-        }
+        gameFragment.setDisplayWord(wordToDisplay);
+
+        hintFragment.setHint("HINT: " + word[1]);
+
         lives = 6;
-        setHangmanImage();
+        gameFragment.setHangmanImage(lives);
     }
-
-    private void setHangmanImage() {
-        switch (lives) {
-            case 6:
-                hangmanView.setImageResource(R.drawable.hangman_0);
-                break;
-            case 5:
-                hangmanView.setImageResource(R.drawable.hangman_1);
-                break;
-            case 4:
-                hangmanView.setImageResource(R.drawable.hangman_2);
-                break;
-            case 3:
-                hangmanView.setImageResource(R.drawable.hangman_3);
-                break;
-            case 2:
-                hangmanView.setImageResource(R.drawable.hangman_4);
-                break;
-            case 1:
-                hangmanView.setImageResource(R.drawable.hangman_5);
-                break;
-            case 0:
-                hangmanView.setImageResource(R.drawable.hangman_6);
-                break;
-            default:
-                hangmanView.setImageResource(R.drawable.hangman_0);
-                break;
-        }
-    }
-
 }
