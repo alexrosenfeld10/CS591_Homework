38989cddf424048c839f13428e9410448692bc75
Tkixi <tkixi@users.noreply.github.com>
2017-10-29 16:49:21 -0400

    finished w7p1

--- HW07/W7_P1/app/src/main/java/com/example/sse/fragmenttransactions_sse/MainActivity.java
+++ HW07/W7_P1/app/src/main/java/com/example/sse/fragmenttransactions_sse/MainActivity.java
@@ -13,7 +13,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-import java.util.ArrayList;
+
+
+// 1b) Broken things: Frag2 and Frag3 Button weren't displaying anything.
+//      Hitting back button would go out of the app.
+//      Re-entering app would reset everything
+//      Fixes: Added all three fragments on the onCreate lifecycle.
+//      Upon clicking the fragment, the current fragment would be replaced
+//      and there would a transaction to add the new fragment to the backstack.
+//      If nothing is in the fragment to begin with, we instantiate a new fragment
+//      from the respective fragment class. We used replace instead of the other possibilities
+//      because of the idea that hiding too many fragments could be costly
+//      if one key feature is to have the back button functional
+//      (many fragments = many backs to go to the start), so removing it completely
+//      could be more effective since these fragments are not costly to add.
 
 public class MainActivity extends AppCompatActivity{
 
@@ -46,25 +59,19 @@ private
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // Check that the activity is using the layout version with
+        // Check that the activity is using the fragment
         if (findViewById(R.id.FragLayout) != null){
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
+            // If restored from a previous state,
+            // just return or else fragments can overlap
             if (savedInstanceState != null) {
                 return;
             }
         }
-
-
         //4. get references for our views.
         btnFrag1 = (Button) findViewById(R.id.btnFrag1);
         btnFrag2 = (Button) findViewById(R.id.btnFrag2);
         btnFrag3 = (Button) findViewById(R.id.btnFrag3);
         FragLayout = (LinearLayout) findViewById(R.id.FragLayout);
-
-//        f1 = (Frag_One) findViewById(R.id.frag1);  //Hey, why won't this work for fragments?  Wait does the fragment even exist?
 
     //5a.  We actually have to create the fragments ourselves.  Where is our friend, R?
         f1 = new Frag_One();
@@ -72,21 +79,17 @@ private
         f3 = new Frag_Three();
 
     //5b. Grab a reference to the Activity's Fragment Manager, Every Activity has one!
-     //  fm = getFragmentManager ();  //that was easy.
-        fm = getSupportFragmentManager();  // **//use this call instead, if your activity extends AppCompatActivity
-        fm.addOnBackStackChangedListener(new android.support.v4.app.FragmentManager.OnBackStackChangedListener() {
-            @Override
-            public void onBackStackChanged() {
-                getFragmentManager().popBackStack();
-            }
-        });
+        // Used getSupportFragmentManager() over getFragmentManager() because we need to
+        // support the Android Frameworks below 5.0
+        fm = getSupportFragmentManager();
+
 
     //5c. Now we can "plop" fragment(s) into our container.
         android.support.v4.app.FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
 
-        ft.add(R.id.FragLayout, f1, "tag1");  //now we have added our fragment to our Activity programmatically.  The other fragments exist, but have not been added yet.
-        ft.add(R.id.FragLayout, f2, "tag2");  //now we have added our fragment to our Activity programmatically.  The other fragments exist, but have not been added yet.
-        ft.add(R.id.FragLayout, f3, "tag3");  //now we have added our fragment to our Activity programmatically.  The other fragments exist, but have not been added yet.
+        ft.add(R.id.FragLayout, f1, "tag1");
+        ft.add(R.id.FragLayout, f2, "tag2");
+        ft.add(R.id.FragLayout, f3, "tag3");
         ft.commit ();  //don't forget to commit your changes.  It is a transaction after all.
 
     btnFrag1.setOnClickListener(new View.OnClickListener() {
@@ -114,47 +117,43 @@ private
 
 public void showFrag1() {
 
+    // Checks if frag even exists, otherwise instantiates new frag
     if (f1 == null)
         f1 = new Frag_One();
+    f1 = (Frag_One) fm.findFragmentByTag("tag1");
 
-    f1 = (Frag_One) fm.findFragmentByTag("tag1");   //what should we do if f1 doesn't exist anymore?  How do we check and how do we fix?
     android.support.v4.app.FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
-    //ft.add(R.id.FragLayout, f1, "tag1");
+    // Adds onto the backstack so that the back button knows what fragment to back on
     ft.addToBackStack ("myFrag1");
+    //
     ft.replace(R.id.FragLayout, f1);
-//    ft.hide(f2);
-//    ft.hide(f3);
-//    ft.show(f1);   //why does this not *always* crash?
     ft.commit();
 }
 
     public void showFrag2() {
-
+        // Checks if frag even exists, otherwise instantiates new frag
         if (f2 == null)
           f2 = new Frag_Two();
-        f2 = (Frag_Two) fm.findFragmentByTag("tag2");   //what should we do if f1 doesn't exist anymore?  How do we check and how do we fix?
+        f2 = (Frag_Two) fm.findFragmentByTag("tag2");
 
         android.support.v4.app.FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
-        ft.addToBackStack ("myFrag2");  //why do we do this?
+        // Adds onto the backstack so that the back button knows what fragment to back on
+        ft.addToBackStack ("myFrag2");
         ft.replace(R.id.FragLayout, f2);
-//
-//        ft.hide(f1);
-//        ft.hide(f3);
-//        ft.show(f2);
         ft.commit();
     }
 
 
     public void showFrag3() {
+        // Checks if frag even exists, otherwise instantiates new frag
         if (f3 == null)
             f3 = new Frag_Three();
-        f3 = (Frag_Three) fm.findFragmentByTag("tag3");   //what should we do if f1 doesn't exist anymore?  How do we check and how do we fix?
+        f3 = (Frag_Three) fm.findFragmentByTag("tag3");
+
         android.support.v4.app.FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
-        ft.replace(R.id.FragLayout, f3);
+        // Adds onto the backstack so that the back button knows what fragment to back on
         ft.addToBackStack ("myFrag3");
-//        ft.hide(f2);
-//        ft.hide(f1);
-//        ft.show(f3);
+        ft.replace(R.id.FragLayout, f3);
         ft.commit();
     }
 }
