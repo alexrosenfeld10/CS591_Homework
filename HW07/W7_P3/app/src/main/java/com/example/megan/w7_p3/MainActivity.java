2dff757b1a750bcece9867ae7b6719d4e2ef56b5
Megan Fantes <meganfantes@gmail.com>
2017-10-29 15:28:40 -0400

    got ratings to work, added menu options

--- HW07/W7_P3/app/src/main/java/com/example/megan/w7_p3/MainActivity.java
+++ HW07/W7_P3/app/src/main/java/com/example/megan/w7_p3/MainActivity.java
@@ -1,41 +1,28 @@
 package com.example.megan.w7_p3;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-//Step-By-Step, Fragment Transactions
+// Step-by-Step Fragment Transactions
 
     private
     ListView lvEpisodes;     //Reference to the listview GUI component
@@ -47,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvEpisodes = (ListView)findViewById(R.id.lvEpisodes);
-        lvAdapter = new MyCustomAdapter(this.getBaseContext());  //instead of passing the boring default string adapter, let's pass our own, see class MyCustomAdapter below!
+        //instead of passing the boring default string adapter, let's pass our own, see class MyCustomAdapter below!
+        lvAdapter = new MyCustomAdapter(this.getBaseContext());
         lvEpisodes.setAdapter(lvAdapter);
 
     }
@@ -61,21 +49,62 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        // get id of menu item clicked
         int id = item.getItemId();
 
-        if (id == R.id.mnu_zero) {
-            Toast.makeText(getBaseContext(), "Menu Zero.", Toast.LENGTH_LONG).show();
-            return true;
-        }
-
+        // if item one clicked, go to start trek merch website
         if (id == R.id.mnu_one) {
-            Toast.makeText(getBaseContext(), "Ring ring, Hi Mom.", Toast.LENGTH_LONG).show();
+            // the website in the assignment doesn't exist, show a toast with a message
+            Toast.makeText(getBaseContext(),
+                    "http://shop.startrek.com/info.php does not exist, so I am sending you to shop.startrek.com",
+                    Toast.LENGTH_LONG).show();
+            // and instead show a different website
+            String url = "http://shop.startrek.com";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
             return true;
         }
 
+        // if item two clicked, dial 1-800-STARTRK (but don't call)
+        if (id == R.id.mnu_two) {
+            String phoneNo = "tel:1800startrk";
+            Intent phoneCall = new Intent(Intent.ACTION_DIAL);
+            phoneCall.setData(Uri.parse(phoneNo));
+            startActivity(phoneCall);
+            return true;
+        }
 
-        return super.onOptionsItemSelected(item);  //if none of the above are true, do the default and return a boolean.
+        // if item three clicked, spawn text message with body "Ouch!" (with no receiving number)
+        if (id == R.id.mnu_three) {
+            try {
+                String message = "Ouch!";
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.putExtra("sms_body", message);
+                sendIntent.setType("vnd.android-dir/mms-sms");
+                startActivity(sendIntent);
+
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(),
+                        "SMS faild, please try again later!",
+                        Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        // if item four clicked, play audio of "Live Long and Prosper"
+        if (id == R.id.mnu_four) {
+            return true;
+        }
+
+        // if item five clicked, play video of "KAHN" scene
+        if (id == R.id.mnu_five) {
+            return true;
+        }
+
+        //if none of the above are true, do the default and return a boolean.
+        return super.onOptionsItemSelected(item);
     }
 }
 
@@ -87,8 +116,8 @@ class MyCustomAdapter extends BaseAdapter {
     String episodes[];
     String  episodeDescriptions[];
     ArrayList<Integer> episodeImages;
-    float ratings[];
 
+    // create a variable for the context so we can use it later
     Context context;
 
     //STEP 2: Override the Constructor, be sure to:
@@ -96,12 +125,14 @@ class MyCustomAdapter extends BaseAdapter {
     // load the strings and images into object references.
     public MyCustomAdapter(Context aContext) {
         //initializing our data in the constructor.
-        context = aContext;  //saving the context we'll need it again.
+        context = aContext;  //saving the context we'll need it again (for intents)
 
-        episodes =aContext.getResources().getStringArray(R.array.episodes);  //retrieving list of episodes predefined in strings-array "episodes" in strings.xml
+        //retrieving list of episodes predefined in strings-array "episodes" in strings.xml
+        episodes =aContext.getResources().getStringArray(R.array.episodes);
         episodeDescriptions = aContext.getResources().getStringArray(R.array.episode_descriptions);
 
-        episodeImages = new ArrayList<Integer>();   //Could also use helper function "getDrawables(..)" below to auto-extract drawable resources, but keeping things as simple as possible.
+        // building ArrayList of apisode images
+        episodeImages = new ArrayList<Integer>();
         episodeImages.add(R.drawable.st_spocks_brain);
         episodeImages.add(R.drawable.st_arena__kirk_gorn);
         episodeImages.add(R.drawable.st_this_side_of_paradise__spock_in_love);
@@ -109,41 +140,36 @@ class MyCustomAdapter extends BaseAdapter {
         episodeImages.add(R.drawable.st_platos_stepchildren__kirk_spock);
         episodeImages.add(R.drawable.st_the_naked_time__sulu_sword);
         episodeImages.add(R.drawable.st_the_trouble_with_tribbles__kirk_tribbles);
-
-        ratings = retrieveSharedPreferenceInfo();
     }
 
-    //STEP 3: Override and implement getCount(..), ListView uses this to determine how many rows to render.
+//STEP 3: Override and implement getCount(..), ListView uses this to determine how many rows to render.
     @Override
     public int getCount() {
-//        return episodes.size(); //all of the arrays are same length, so return length of any... ick!  But ok for now. :)
-        return episodes.length;   //all of the arrays are same length, so return length of any... ick!  But ok for now. :)
-        //Q: How else could we have done this (better)? ________________
+        return episodes.length;   //all of the arrays are same length
     }
 
-    //STEP 4: Override getItem/getItemId, we aren't using these, but we must override anyway.
+//STEP 4: Override getItem/getItemId, we aren't using these, but we must override anyway (base is abstract)
     @Override
     public Object getItem(int position) {
-//        return episodes.get(position);  //In Case you want to use an ArrayList
-        return episodes[position];        //really should be returning entire set of row data, but it's up to us, and we aren't using this call.
+        return episodes[position];
     }
 
     @Override
     public long getItemId(int position) {
-        return position;  //Another call we aren't using, but have to do something since we had to implement (base is abstract).
+        return position;
     }
 
-    //THIS IS WHERE THE ACTION HAPPENS.  getView(..) is how each row gets rendered.
+//THIS IS WHERE THE ACTION HAPPENS.  getView(..) is how each row gets rendered.
 //STEP 5: Easy as A-B-C
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {  //convertView is Row (it may be null), parent is the layout that has the row Views.
+    //convertView is Row (it may be null), parent is the layout that has the row Views.
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
 //STEP 5a: Inflate the listview row based on the xml.
-        View row;  //this will refer to the row to be inflated or displayed if it's already been displayed. (listview_row.xml)
-//        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        row = inflater.inflate(R.layout.listview_row, parent, false);  //
+        //this will refer to the row to be inflated or displayed if it's already been displayed. (listview_row.xml)
+        View row;
 
-// Let's optimize a bit by checking to see if we need to inflate, or if it's already been inflated...
+        // Let's optimize a bit by checking to see if we need to inflate, or if it's already been inflated.
         if (convertView == null){  //indicates this is the first time we are creating this row.
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             row = inflater.inflate(R.layout.listview_row, parent, false);
@@ -153,17 +179,21 @@ class MyCustomAdapter extends BaseAdapter {
             row = convertView;
         }
 
-//STEP 5b: Now that we have a valid row instance, we need to get references to the views within that row and fill with the appropriate text and images.
-        ImageView imgEpisode = (ImageView) row.findViewById(R.id.imgEpisode);  //Q: Notice we prefixed findViewByID with row, why?  A: Row, is the container.
+//STEP 5b: Now that we have a valid row instance, we need to get references to the views
+
+        //we prefixed findViewByID with row, because row is the container.
+        ImageView imgEpisode = (ImageView) row.findViewById(R.id.imgEpisode);
         TextView tvEpisodeTitle = (TextView) row.findViewById(R.id.tvEpisodeTitle);
         TextView tvEpisodeDescription = (TextView) row.findViewById(R.id.tvEpisodeDescription);
         RatingBar ratingBar = (RatingBar) row.findViewById(R.id.rbEpisode);
 
+        // fill with the appropriate text and images.
         tvEpisodeTitle.setText(episodes[position]);
         tvEpisodeDescription.setText(episodeDescriptions[position]);
         imgEpisode.setImageResource(episodeImages.get(position).intValue());
-        ratingBar.setRating(ratings[position]);
+        ratingBar.setRating(retrieveSharedPreferenceInfo(position));
 
+        // when image is clicked, open a website (Wikipedia of Start Trek episodes)
         imgEpisode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -174,6 +204,7 @@ class MyCustomAdapter extends BaseAdapter {
             }
         });
 
+        // when episode title is clicked, open a website (Wikipedia of Start Trek episodes)
         tvEpisodeTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -184,32 +215,36 @@ class MyCustomAdapter extends BaseAdapter {
             }
         });
 
+        // make sure the ratings for each episode "stick"
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                if (fromUser = true) {
+                if (fromUser == true) {
                     saveSharedPreferenceInfo(rating, position);
                 }
             }
         });
 
 //STEP 5c: That's it, the row has been inflated and filled with data, return it.
-        return row;  //once the row is fully constructed, return it.
+        return row;
 
     }
 
-    void saveSharedPreferenceInfo(float rating, int id){
+    // We want the rating for each episode to "stick," so we create SharedPreferences
+    // this means the rating will remain even when we close the app, and they will be restored when we open it again
+    void saveSharedPreferenceInfo(float rating, int position){
         //1. Refer to the SharedPreference Object.
-        SharedPreferences ratingsPreferences = context.getSharedPreferences("RatingsPreferences", Context.MODE_PRIVATE);  //Private means no other Apps can access this.
+        // (Private means no other Apps can access this.)
+        SharedPreferences ratingsPreferences = context.getSharedPreferences("RatingsPreferences", Context.MODE_PRIVATE);
 
         //2. Create an Shared Preferences Editor for Editing Shared Preferences.
-        //Note, not a real editor, just an object that allows editing...
-
         SharedPreferences.Editor editor = ratingsPreferences.edit();
 
+        // create variably to hold the string for the key for the key, value pair
         String key = "";
 
-        switch (id) {
+        // depending on which rating (value) was changed, choose the appropriate key
+        switch (position) {
             case 0:
                 key = "rbRatings0";
                 break;
@@ -233,66 +268,46 @@ class MyCustomAdapter extends BaseAdapter {
                 break;
         }
 
-        //3. Store what's important!  Key Value Pair, what else is new...
+        //3. Store what's important!  (Key, Value Pair)
         editor.putFloat(key, rating);
+
         //4. Save your information.
         editor.apply();
-
-
-        // for debugging: which rating was just updated
-        Toast.makeText(context, key, Toast.LENGTH_SHORT).show();
     }
 
-
-    float[] retrieveSharedPreferenceInfo(){
+    // when we open the app, we want the ratings that we saved to be restored
+    float retrieveSharedPreferenceInfo(int position){
         SharedPreferences ratingsPreferences = context.getSharedPreferences("RatingsPreferences", Context.MODE_PRIVATE);
 
-//        String key = "";
-//
-//        //Retrieving data from shared preferences hashmap.
-//
-//        switch (position) {
-//            case 1:
-//                key = "rbRatings1";
-//                break;
-//            case 2:
-//                key = "rbRatings2";
-//                break;
-//            case 3:
-//                key = "rbRatings3";
-//                break;
-//            case 4:
-//                key = "rbRatings4";
-//                break;
-//            case 5:
-//                key = "rbRatings5";
-//                break;
-//            case 6:
-//                key = "rbRatings6";
-//                break;
-//            case 7:
-//                key = "rbRatings7";
-//                break;
-//        }
+        // create variable to hold key for key, value pair
+        String key = "";
 
-        float r0 = ratingsPreferences.getFloat("rbRatings0", 0);
-        float r1 = ratingsPreferences.getFloat("rbRatings1", 0);
-        float r2 = ratingsPreferences.getFloat("rbRatings2", 0);
-        float r3 = ratingsPreferences.getFloat("rbRatings3", 0);
-        float r4 = ratingsPreferences.getFloat("rbRatings4", 0);
-        float r5 = ratingsPreferences.getFloat("rbRatings5", 0);
-        float r6 = ratingsPreferences.getFloat("rbRatings6", 0);
+        // depending on which rating we are restoring, select that key
+        switch (position) {
+            case 0:
+                key = "rbRatings0";
+                break;
+            case 1:
+                key = "rbRatings1";
+                break;
+            case 2:
+                key = "rbRatings2";
+                break;
+            case 3:
+                key = "rbRatings3";
+                break;
+            case 4:
+                key = "rbRatings4";
+                break;
+            case 5:
+                key = "rbRatings5";
+                break;
+            case 6:
+                key = "rbRatings6";
+                break;
+        }
 
-//        float r0 = 1;
-//        float r1 = 2;
-//        float r2 = 3;
-//        float r3 = 4;
-//        float r4 = 5;
-//        float r5 = 4;
-//        float r6 = 3;
-
-        float[] ratings = new float[] {r0,r1,r2,r3,r4,r5,r6};
-
-        return ratings;
+        // return the rating
+        return ratingsPreferences.getFloat(key, 0);
     }
 }
